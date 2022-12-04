b83751c833af0a92e58aa50ab9b975b5c7e5a82e
AnnZ <nutazorro369@gmail.com>
2022-12-05 00:09:33 +0700

    add solution for task 0,1,3,5,7

---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,9 @@
+﻿//Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+   //остаток от деления
+    int lastDigit = inputNumber % 10;
+    Console.WriteLine(lastDigit);
+}
