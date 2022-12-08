25de6d0864d4eb1161920a90b93f9ef3cf3c36ed
AnnZ <nutazorro369@gmail.com>
2022-12-09 03:52:25 +0700

    add homework2 task 10,13,15

---
+++ Homework2Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+string? inputLine = Console.ReadLine(); //считывание данных
+int inputNumber = Convert.ToInt32(inputLine); 
+string stNumber = Convert.ToString(inputNumber);//конвертация в строку
+Console.WriteLine("вторая цифра этого числа -> " + stNumber[1]); //вывод данных
+
+
---
+++ Homework2Task13/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+//Считываем данные с консоли
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()??"0");
+ //создаем цикл, при наличии 3 цифр
+ if (number>99)
+{
+    //подсчет количества цифр в числе, с помощью логарифма
+    int length = (int)Math.Log10(number)+1;
+
+    int index = 0;
+    int thirdDigit = number;
+    //деление числа на 10 раз
+    while (index < length-3)
+    {
+        thirdDigit = thirdDigit/10;
+        index++;
+    }
+    thirdDigit = thirdDigit%10; //находим 3 цифру остатком от деления на 10
+    Console.WriteLine(thirdDigit + " третья цифра числа " + number);
+}
+else //если в числе меньше 3 цифр
+{
+    Console.WriteLine("-> третьей цифры нет");   
+}
