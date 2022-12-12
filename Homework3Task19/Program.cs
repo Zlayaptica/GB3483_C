ef6f76965df5c9ed37beb0822958e576a9106f25
AnnZ <nutazorro369@gmail.com>
2022-12-13 00:41:17 +0700

    add homework2 task 19,21,23

---
+++ Homework3Task19/Program.cs
@@ -0,0 +1,90 @@
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+// Console.WriteLine("Введите число: "); //ввод данных
+// string number = Console.ReadLine();
+// int lenNumber = number.Length;
+
+// if (lenNumber == 5)
+// {
+//     if (number[0] == number[4]||number[1] == number[3]) //проверка равенства значений
+//     {
+//         Console.WriteLine($"{number} - Палиндром"); //вывод данных
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{number} - НЕ палиндром");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+// }
+
+
+//ввод данных
+int outputNumber = ReadData("Введите пятизначное число ");
+//проверка корректности введенных данных
+if (outputNumber>=10000&&outputNumber<=99999)
+{
+    //создаем словарь
+    var palindrome = new Dictionary<int, int>();
+    palindrome = DictionaryFill();
+    //проверка наличия данных в словаре
+    bool chek = PalinTest(outputNumber, palindrome);
+    if (chek)
+    {
+        PrintData(outputNumber, " palindrome");
+    }
+    else
+    {
+        PrintData(outputNumber, " not palindrome");
+    }
+}
+else
+{
+    Console.WriteLine("Введенные данные не корректны");
+}
+//метод считывания данных
+int ReadData(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()??"0");
+}
+//метод вывода результата
+void PrintData(int num, string msg)
+{
+    Console.WriteLine(num+msg);
+}
+//метод fill извлекает из словаря данные
+Dictionary<int, int> DictionaryFill()
+{
+    var dictionary = new Dictionary<int, int>();
+    int keyNumber = 1;
+        for(int secondNumber = 0; secondNumber<=9; secondNumber++)
+        {
+            for (int firstNumber = 1; firstNumber<=9; firstNumber++)
+            {
+                dictionary[keyNumber] = firstNumber*1000+secondNumber*100+secondNumber*10+firstNumber;
+                keyNumber++;
+            }
+        }
+    return dictionary;
+}
+//метод проверяет, если 5 чисел палиндром
+bool PalinTest(int number,Dictionary<int, int> palindrome)
+{
+    bool result = false;
+    if (palindrome.ContainsValue((number/1000)*100+(number%100)))
+    {
+        result = true;
+    }
+    return result;
+}
---
+++ Homework3Task23/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+//ввод данных
+int inputNum = ReadData("Введите число ");
+string[] firstRow = LineBuild(inputNum,1).Split(" ");
+string[] secondRow = LineBuild(inputNum,3).Split(" ");
+Console.Write(" ");
+for (int i=1; i<=LineBuild(inputNum,3).Length;i++) {Console.Write("_");}
+Console.Write("\n");
+string[] space = new string[inputNum];
+for(int num=0; num<inputNum;num++)
+{
+    for (int i=1; i<=(secondRow[num].Length-firstRow[num].Length);i++)
+    {
+        space[num] = space[num] + " ";
+    }
+    firstRow[num] = space[num]+firstRow[num];
+}
+string[] line = new string[inputNum];
+for(int num=0; num<inputNum; num++)
+{
+   line[num]=String.Concat(Enumerable.Repeat("_",secondRow[num].Length)); 
+}
+PrintData(firstRow);
+PrintData(line);
+PrintData(secondRow);
+PrintData(line);
+
+int ReadData(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()??"0");
+}
+string LineBuild(int num, int pow)
+{
+    string line = string.Empty;
+    for (int i=1; i<num; i++)
+    {
+        line = line+Math.Pow(i,pow)+ " ";
+    }
+    line = line + Math.Pow(num,pow);
+    return line;
+}
+void PrintData(string[] line)
+{
+    foreach (var num in line)
+    {
+        Console.Write($"|{num}");
+    }
+    Console.WriteLine("|");
+}
+
+
+
+// //ввод данных
+// Console.WriteLine(line);
+// //считываем данные
+// int ReadData(string msg)
+// {
+//     Console.WriteLine(msg);
+//     return int.Parse(Console.ReadLine()??"0");
+// }
+// string lineBuilder(int n)
+// {
+//     string line = "";
+//     string line2 = "";
+//     for (int i = 1; i<n; i++){
+
+//     }
+// }
